2ffcee3fcba671e07aa0e167c83db25505dd1056
Pasha Kornya <pashakornya@gmail.com>
2018-04-28 00:29:46 +0300

    Basic configurations

---
+++ imagecollection.cpp
@@ -0,0 +1,6 @@
+#include "imagecollection.h"
+
+ImageCollection::ImageCollection()
+{
+
+}
---
+++ imagecollection.h
@@ -0,0 +1,11 @@
+#ifndef IMAGECOLLECTION_H
+#define IMAGECOLLECTION_H
+
+
+class ImageCollection
+{
+public:
+    ImageCollection();
+};
+
+#endif // IMAGECOLLECTION_H
