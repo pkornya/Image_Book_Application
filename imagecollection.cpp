bfaf6a7453b35ec703daa4f33d0cfc6a79d29d61
Pasha Kornya <pashakornya@gmail.com>
2018-04-29 01:33:06 +0300

    Started working with database

--- imagecollection.cpp
+++ imagecollection.cpp
@@ -1,6 +1,56 @@
+#include <QtSql>
+
 #include "imagecollection.h"
 
 ImageCollection::ImageCollection()
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+
+    db.setDatabaseName(":memory");
+    if (!db.open())
+        qFatal("Failed to open database");
+
+    populateDatabase();
+}
+
+void ImageCollection::populateDatabase()
+{
+    QSqlQuery qry;
+
+    qry.prepare("CREATE TABLE IF NOT EXISTS images "
+                "(id INTEGER PRIMARY KEY, data BLOB)");
+    if (!qry.exec())
+        qFatal("Failed to create table images");
+
+    qry.prepare("CREATE TABLE IF NOT EXISTS tags (id INTEGER, tag VARCHAR(30))");
+    if (!qry.exec())
+        qFatal("Failed to create table tags");
 
 }
+
+QImage ImageCollection::getImage(int id)
+{
+
+}
+
+QList<int> ImageCollection::getIds(QStringList tags)
+{
+
+}
+
+QStringList ImageCollection::getTags()
+{
+
+}
+
+void ImageCollection::addTag(int id, QString tag)
+{
+
+}
+
+void ImageCollection::addImage(QImage image, QStringList tags)
+{
+
+}
+
+
--- imagecollection.h
+++ imagecollection.h
@@ -1,10 +1,22 @@
 #ifndef IMAGECOLLECTION_H
 #define IMAGECOLLECTION_H
 
+#include <QDialog>
+
 class ImageCollection
 {
 public:
     ImageCollection();
+
+    QImage getImage(int id);
+    QList<int> getIds(QStringList tags);
+    QStringList getTags();
+
+    void addTag(int id, QString tag);
+    void addImage(QImage image, QStringList tags);
+
+private:
+    void populateDatabase();
 };
 
 #endif // IMAGECOLLECTION_H
